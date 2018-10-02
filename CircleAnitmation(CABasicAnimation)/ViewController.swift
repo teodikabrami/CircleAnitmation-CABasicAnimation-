25852c4f76550c2159e4626e6476e6b75f153c3a
Apple <teodikabrami@gmail.com>
2018-10-02 15:26:40 +0330

    Initial Commit

---
+++ CircleAnitmation(CABasicAnimation)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleAnitmation(CABasicAnimation)
+//
+//  Created by Teodik Abrami on 10/2/18.
+//  Copyright © 2018 Teodik Abrami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
